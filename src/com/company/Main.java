a419be10155fdc43ca50bb90d2159bc6c6fa9a6b
Nick <nixfoxy@gmail.com>
2016-06-24 15:52:47 +0300

    Закомітив файл з перебором масиву

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-    Dog labrador = new Dog();
-        labrador.setName("Pluto");
-        labrador.setYears(2);
-        labrador.setSize(44);
-        labrador.bark(1);
 
+        int [] index = new int [4];
+        index [0] = 1;
+        index [1] = 3;
+        index [2] = 0;
+        index [3] = 2;
+
+        String [] islands = new String[4] ;
+        islands [0] = "Бермуды";
+        islands [1] = "Фиджи";
+        islands [2] = "Азорские острова";
+        islands [3] = "Косумель";
+
+        int y = 0;
+        int ref;
+        while (y < 4) {
+            ref = index[y];
+            System.out.print("Острова = ");
+            System.out.println(islands[ref]);
+            y = y+1;
+        }
     }
 }
--- src/com/company/masive.java
+++ src/com/company/masive.java
@@ -4,4 +4,23 @@ package com.company;
  * Created by Nick on 6/24/16.
  */
 public class masive {
+    int [] index = new int [4];
+    index [0] = 1;
+    index [1] = 3;
+    index [2] = 0;
+    index [3] = 2;
+
+    String [] islands = new String[4] ;
+    islands [0] = "Бермуды";
+    islands [1] = "Фиджи";
+    islands [2] = "Азорские острова";
+    islands [3] = "Косумель";
+
+    int y = 0;
+    int ref;
+    while (y < 4) {
+        ref = index[y];
+        System.out.print("Острова = ");
+        System.out.println(islands[ref]);
+        y = y+1;
 }
